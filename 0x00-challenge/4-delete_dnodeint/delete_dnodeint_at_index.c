c4607f65650fc001cad87e8299c2a0e2cbd92825
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-06-03 05:44:28 +0300

    challenge

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,38 +11,38 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    unsigned int i = 0;
+	dlistint_t *current = *head;
+	unsigned int i = 0;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    /* Traverse to the node at the specified index */
-    while (current != NULL && i < index)
-    {
-        current = current->next;
-        i++;
-    }
+	/* Traverse to the node at the specified index */
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
 
-    /* If the node at the specified index does not exist */
-    if (current == NULL)
-        return (-1);
+	/* If the node at the specified index does not exist */
+	if (current == NULL)
+		return (-1);
 
-    /* If the node to be deleted is the head node */
-    if (current == *head)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-    }
-    else
-    {
-        if (current->prev != NULL)
-            current->prev->next = current->next;
-        if (current->next != NULL)
-            current->next->prev = current->prev;
-    }
+	/* If the node to be deleted is the head node */
+	if (current == *head)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		if (current->prev != NULL)
+			current->prev->next = current->next;
+		if (current->next != NULL)
+			current->next->prev = current->prev;
+	}
 
-    free(current);
-    return (1);
+	free(current);
+	return (1);
 }
